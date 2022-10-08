2e218345bb80dd289d60090bf5e6bc694e4d6eb3
Mauricio Avellar <mbavellar@gmail.com>
2022-10-07 23:22:21 -0300

    Projeto Criado

---
+++ src/main/java/com/mbavellar/coursesb/CoursesbApplication.java
@@ -0,0 +1,13 @@
+package com.mbavellar.coursesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesbApplication.class, args);
+	}
+
+}
