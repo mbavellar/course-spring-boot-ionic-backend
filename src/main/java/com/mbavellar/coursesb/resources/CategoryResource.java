4731a74bee724d49a2f5aa68bcf9fcc2e34fa902
Mauricio Avellar <mbavellar@gmail.com>
2022-10-11 14:26:54 -0300

    Category Domain Class

--- src/main/java/com/mbavellar/coursesb/resources/CategoryResource.java
+++ src/main/java/com/mbavellar/coursesb/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.mbavellar.coursesb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mbavellar.coursesb.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
   @RequestMapping(method = RequestMethod.GET)
-  public String test() {
-	  return "Rest is working!";
+  public List<Category> test() {
+	  
+	  Category cat1 = new Category(1, "IT");
+	  Category cat2 = new Category(1, "Office");
+	  
+	  List<Category> categories = new ArrayList<>();
+	  categories.add(cat1);
+	  categories.add(cat2);
+	  
+	  return categories;
   }
 }
