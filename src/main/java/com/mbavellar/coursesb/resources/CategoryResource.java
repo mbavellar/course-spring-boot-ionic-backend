139a212917bba522b429237f4e058c6881429944
Mauricio Avellar <mbavellar@gmail.com>
2022-10-20 00:40:12 -0300

    Category Repository & Service Created

--- src/main/java/com/mbavellar/coursesb/resources/CategoryResource.java
+++ src/main/java/com/mbavellar/coursesb/resources/CategoryResource.java
@@ -1,28 +1,26 @@
 package com.mbavellar.coursesb.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mbavellar.coursesb.domain.Category;
+import com.mbavellar.coursesb.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
-  @RequestMapping(method = RequestMethod.GET)
-  public List<Category> test() {
-	  
-	  Category cat1 = new Category(1, "IT");
-	  Category cat2 = new Category(1, "Office");
-	  
-	  List<Category> categories = new ArrayList<>();
-	  categories.add(cat1);
-	  categories.add(cat2);
-	  
-	  return categories;
+  @Autowired
+  private CategoryService categoryService;
+	
+  @RequestMapping(value="/{id}", method = RequestMethod.GET)
+  public ResponseEntity<?> test(@PathVariable Integer id) {
+	  Category category = categoryService.findById(id);
+	  return ResponseEntity.ok().body(category);
   }
 }
---
+++ src/main/java/com/mbavellar/coursesb/services/CategoryService.java
@@ -0,0 +1,17 @@
+package com.mbavellar.coursesb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mbavellar.coursesb.domain.Category;
+import com.mbavellar.coursesb.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository; 
+	public Category findById(Integer id) {
+		return categoryRepository.findById(id).orElse(null);
+	}
+}
