8e290c23edbde54bb0a2ad1be9854f2c56c5ab23
Mauricio Avellar <mbavellar@gmail.com>
2022-10-23 16:06:37 -0300

    Adding Instantiation Operation

--- src/main/java/com/mbavellar/coursesb/CoursesbApplication.java
+++ src/main/java/com/mbavellar/coursesb/CoursesbApplication.java
@@ -1,13 +1,33 @@
 package com.mbavellar.coursesb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoursesbApplication {
+import com.mbavellar.coursesb.domain.Category;
+import com.mbavellar.coursesb.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CoursesbApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursesbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "IT");
+		Category cat2 = new Category(null, "Office");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
